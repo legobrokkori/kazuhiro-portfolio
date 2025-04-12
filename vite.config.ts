ae160207f6cb0e9be8f364b0ae863ce9f580d6f5
Kazuhiro Watanabe <ralirulego103@gmail.com>
2025-04-13 03:44:39 +0900

    fix: move index.html to root for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // ↓ export default ではなく module.exports にするとNodeに確実に読まれる
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
   }
 })
